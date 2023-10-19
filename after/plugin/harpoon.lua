4c0db1d11f87019894739ef71915aaed8fd0b825
nikole <nikole@46elks.com>
2023-10-19 10:13:41 +0200

    add terminal shortcut

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -1,5 +1,6 @@
 local mark = require("harpoon.mark")
 local ui = require("harpoon.ui")
+local term = require("harpoon.term")
 
 vim.keymap.set("n", "<leader>a", mark.add_file)
 vim.keymap.set("n", "<leader>ee", ui.toggle_quick_menu)
@@ -9,3 +10,5 @@ vim.keymap.set("n", "<A-l>", function() ui.nav_file(1) end)
 vim.keymap.set("n", "<A-u>", function() ui.nav_file(2) end)
 vim.keymap.set("n", "<A-y>", function() ui.nav_file(3) end)
 vim.keymap.set("n", "<A-;>", function() ui.nav_file(4) end)
+
+vim.keymap.set("n", "<A-t>", function() term.gotoTerminal(1) end)
